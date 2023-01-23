95b26f7afaceeda8a2da8c841a6cd115eabba637
Dryelle Ebelin <dryellesilva07@gmail.com>
2023-01-22 21:14:21 -0300

    projeto

---
+++ botao-light-dark-mode/script.js
@@ -0,0 +1,33 @@
+function changeMode() {
+    changeClasses();
+    changeText();
+}
+
+function changeClasses() {   //muda a classe
+    button.classList.toggle(darkModeClass);
+    h1.classList.toggle(darkModeClass);
+    body.classList.toggle(darkModeClass);
+    footer.classList.toggle(darkModeClass);
+}
+
+function changeText() {  //muda o texto
+    const lightMode = "Light Mode";
+    const darkMode = "Dark Mode";
+
+    if(body.classList.contains(darkModeClass)) {
+        button.innerHTML = lightMode;
+        h1.innerHTML = darkMode + ' ON';
+        return;
+    }
+
+    button.innerHTML = darkMode;
+    h1.innerHTML = lightMode + ' ON';
+}
+
+const darkModeClass = 'dark-mode';
+const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('page-title');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
+button.addEventListener('click', changeMode);
