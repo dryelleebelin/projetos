a5512a38046ebae85023db81ec043cc8907b902e
dryelleebelin <dryellesilva07@gmail.com>
2024-03-19 18:45:59 -0300

    add menu

--- the-rick-and-morty/src/components/Header/index.js
+++ the-rick-and-morty/src/components/Header/index.js
@@ -1,9 +1,14 @@
+import React, { useState } from 'react';
 import './header.scss';
 import logo from '../../images/logo.png';
 import logo2 from '../../images/logo2.png';
 import DarkMode from '../DarkMode/DarkMode';
+import { IoMenu, IoClose } from "react-icons/io5";
+import 'animate.css';
 
 export default function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     const scrollToTop = () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     };
@@ -17,6 +22,14 @@ export default function Header() {
         document.getElementById('seasons').scrollIntoView({ behavior: 'smooth' });
     };
 
+    const menuShow = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const closeMenu = () => {
+        setMenuOpen(false)
+    }
+
     return (
         <header>
             <div onClick={scrollToTop}>
@@ -29,7 +42,28 @@ export default function Header() {
                 <a onClick={scrollToPhrases}>FRASES E MEMES</a>
                 <a onClick={scrollToSeasons}>TEMPORADAS</a>
             </nav>
-            <DarkMode/>
+            <div className='dark-mode'>
+                <DarkMode/>
+            </div>
+
+            <button className='menu-icon' onClick={menuShow}>
+                {menuOpen ? <IoClose className='icon'/> : <IoMenu className='icon'/>}
+            </button>
+
+            {menuOpen && (
+                <>
+                    <span className='close-menu' onClick={closeMenu}/>
+                    <div className='animate__animated animate__fadeInRight nav-menu'>
+                        <a onClick={() => { scrollToTop(); closeMenu(); }}>HOME</a>
+                        <a onClick={() => { scrollToCharacters(); closeMenu(); }}>PERSONAGENS</a>
+                        <a onClick={() => { scrollToPhrases(); closeMenu(); }}>FRASES E MEMES</a>
+                        <a onClick={() => { scrollToSeasons(); closeMenu(); }}>TEMPORADAS</a>
+                        <div className='dark-mode-menu'>
+                            <DarkMode/>
+                        </div>
+                    </div>
+                </>
+            )}
         </header>
     );
 }
