8e998f04b6980c65bcd7d70e15a1d62c8b28d1f0
dryelleebelin <dryellesilva07@gmail.com>
2024-04-01 10:14:53 -0300

    husky config

--- cinestream/src/services/api.js
+++ cinestream/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({
+const api = axios.create(((
   baseURL: 'https://api.themoviedb.org/3/',
   params: {
     api_key: "df4556586815e5b2b94838a7bf5c822b",
