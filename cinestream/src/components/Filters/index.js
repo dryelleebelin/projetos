25f9d6f9272696b54c9f738517fffedf1dcd7f22
dryelleebelin <dryellesilva07@gmail.com>
2024-04-25 17:05:28 -0300

    update

--- cinestream/src/components/Filters/index.js
+++ cinestream/src/components/Filters/index.js
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from 'react'
 import './filters.scss'
 import api from '../../services/api'
 import useTranslations from '../../translations/useTranslations'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 export default function Filters({ onFilterChange }) {
     const translations = useTranslations()
+    const navigate = useNavigate()
     const [genres, setGenres] = useState([])
     const [countries, setCountries] = useState([])
     const [languages, setLanguages] = useState([])
+    const [loading, setLoading] = useState(true)
 
     const [filter, setFilter] = useState({
         genre: '',
@@ -24,13 +28,18 @@ export default function Filters({ onFilterChange }) {
                 api.get('configuration/languages')
             ])
             const combinedGenres = [...moviesResponse.data.genres, ...tvResponse.data.genres]
-            
-            setGenres(filterGenres(combinedGenres))
+            const sortedGenres = combinedGenres.sort((a, b) => a.name.localeCompare(b.name))
+            setGenres(filterGenres(sortedGenres))
+
             setCountries(countriesResponse.data)
-            setLanguages(languagesResponse.data)
+
+            const sortedLanguages = languagesResponse.data.sort((a, b) => a.english_name.localeCompare(b.english_name))
+            setLanguages(sortedLanguages)
+            setLoading(false)
 
         } catch(error){
             console.error(translations.errorLoadingFilters, error)
+            setLoading(false)
         }
     }
 
@@ -42,12 +51,36 @@ export default function Filters({ onFilterChange }) {
 
     function handleChangeFilter(e){
         const {name, value} = e.target
-        setFilter((prevFilter) => ({...prevFilter, [name]: value}))
+        const updatedFilter = {...filter, [name]: value}
+        setFilter(updatedFilter)
+    
         if (onFilterChange) {
-            onFilterChange(filter)
+            onFilterChange(updatedFilter)
         }
-        console.log(filter)
-        console.log(filter.language)
+
+        if (value !== '') {
+            navigate('/filtered-results')
+            toast.info(translations.underDevelopment)
+        }
+    }
+
+    function handleClearFilters(e){
+        e.preventDefault()
+
+        navigate('/catalog')
+
+        setFilter({
+            genre: '',
+            country: '',
+            language: ''
+        })
+        if (onFilterChange) {
+            onFilterChange({
+                genre: '',
+                country: '',
+                language: ''
+            })
+        }
     }
 
     useEffect(() => {
@@ -56,28 +89,30 @@ export default function Filters({ onFilterChange }) {
 
     return (
         <>
-            <form className='container-filters'>
-                <select name='genre' defaultValue="gênero" onChange={handleChangeFilter}>
-                    <option disabled value="gênero">{translations.gender}</option>
-                    {genres.map((genre, index) => (
-                        <option key={index} value={genre.id}>{genre.name}</option>
-                    ))}
-                </select>
-                <select name='country' defaultValue="país" onChange={handleChangeFilter}>
-                    <option disabled value="país">{translations.country}</option>
-                    {countries.map((country, index) => (
-                        <option key={index} value={country.iso_3166_1}>{country.english_name.split(" ").slice(0, 2).join(" ")}</option>
-                    ))}
-                </select>
-                <select name='language' defaultValue="idioma" onChange={handleChangeFilter}>
-                    <option disabled value="idioma">{translations.language}</option>
-                    {languages.map((language, index) => (
-                        <option key={index} value={language.iso_639_1}>{language.english_name.split(" ").slice(0, 1).join(" ")}</option>
-                    ))}
-                </select>
-                <input type='text' name='search' value={filter.search || ''} placeholder={translations.filmSeriesOrPerson} autoComplete='off' onChange={handleChangeFilter} disabled/>
-                <button type='submit' disabled>{translations.toClean}</button>
-            </form>
+            {!loading && (
+                <form className='container-filters'>
+                    <select name='genre' defaultValue="gênero" onChange={handleChangeFilter}>
+                        <option disabled value="gênero">{translations.gender}</option>
+                        {genres.map((genre, index) => (
+                            <option key={index} value={genre.id}>{genre.name}</option>
+                        ))}
+                    </select>
+                    <select name='country' defaultValue="país" onChange={handleChangeFilter} disabled>
+                        <option disabled value="país">{translations.country}</option>
+                        {countries.map((country, index) => (
+                            <option key={index} value={country.iso_3166_1}>{country.english_name.split(" ").slice(0, 2).join(" ")}</option>
+                        ))}
+                    </select>
+                    <select name='language' defaultValue="idioma" onChange={handleChangeFilter} disabled>
+                        <option disabled value="idioma">{translations.language}</option>
+                        {languages.map((language, index) => (
+                            <option key={index} value={language.iso_639_1}>{language.english_name.split(" ").slice(0, 1).join(" ")}</option>
+                        ))}
+                    </select>
+                    <input type='text' name='search' value={filter.search || ''} placeholder={translations.filmSeriesOrPerson} autoComplete='off' onChange={handleChangeFilter} disabled/>
+                    <button type='button' onClick={handleClearFilters}>{translations.toClean}</button>
+                </form>
+            )}
         </>
     )
 }
--- cinestream/src/pages/Filtered/index.js
+++ cinestream/src/pages/Filtered/index.js
@@ -1,45 +1,72 @@
 import React, { useEffect, useState } from "react"
 import './filtered.scss'
 import api from "../../services/api"
+import { useNavigate } from "react-router-dom"
+import { toast } from "react-toastify"
+import useTranslations from "../../translations/useTranslations"
 
 import Header from "../../components/Header"
 import Filters from "../../components/Filters"
 import Footer from "../../components/Footer"
 
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
+import { CgSpinner } from "react-icons/cg"
+
 export default function Filtered(){
-  const [filteredMovies, setFilteredMovies] = useState([])
+  const translations = useTranslations()
+  const navigate = useNavigate()
+  const [filtered, setFiltered] = useState([])
+  const [page, setPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(30)
+  const [loading, setLoading] = useState(true)
 
   async function fetchResults(filters){
     try{
-      const response = await api.get('discover/movie', {
-        params: {
-          with_genres: filters.genre,
-          'region': filters.country,
-          'language': filters.language
-        }
-      })
-      setFilteredMovies(response.data.results)
-      console.log(response.data.results)
+      const [movieResponse, tvResponse] = await Promise.all([
+        api.get('discover/movie', {params: { page: page, with_genres: filters.genre }}),
+        api.get('discover/tv', {params: { page: page, with_genres: filters.genre }})
+      ])
+
+      const combinedResults = [...movieResponse.data.results, ...tvResponse.data.results]
+      
+      setTimeout(() => {
+        setFiltered(combinedResults)
+        setLoading(false)
+      }, 1000)
 
     } catch(error){
-      console.error('Erro ao buscar resultados do filtro: ', error)
+      toast.error(translations.anErrorOccurredWhileFetchingTheResultsTryAgainLater)
+      console.error(translations.errorWhenFetchingFilterResults, error)
+      setLoading(false)
     }
   }
 
   useEffect(() => {
     fetchResults({})
-  }, [])
+  }, [page])
 
   return(
     <>
       <Header/>
       <Filters onFilterChange={fetchResults}/>
 
-      <main className="filtered">
-        {filteredMovies.map(item => (
-          <img key={item.id} src={`https://image.tmdb.org/t/p/original/${item.poster_path}`} alt="Cover"/>
-        ))}
-      </main>
+      {loading ? (
+        <div className="spinner-vh spinner"><CgSpinner/></div>
+      ) : (
+        <main className="filtered">
+          <section>
+            {filtered.map(item => (
+              <img key={item.id} src={`https://image.tmdb.org/t/p/original/${item.poster_path}`} alt="Cover" onClick={() => { window.scrollTo({top: 0, behavior: 'smooth'}); navigate(`/detail/${item.id}`) }}/>
+            ))}
+          </section>
+
+          <div className='pagination'>
+            <button type="button" onClick={() => setPage(page - 1)} disabled={page === 1}><FaChevronLeft/></button>
+            <p>{page} / {totalPages}</p>
+            <button type="button" onClick={() => setPage(page + 1)} disabled={page === totalPages}><FaChevronRight/></button>
+          </div>
+        </main>
+      )}
 
       <Footer/>
     </>
